1427f5aebaad068a5af3c1d9eea81826fc2f7705
MaheuTaroo <pantupa@gmail.com>
2021-02-16 23:44:35 +0000

    Addition of logical project

---
+++ SupaPlayerLib/MediaTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupaPlayerLib
+{
+    public static class MediaTools
+    {
+        public static List<string> Media { get; set; } = new List<string>();
+        public static string Current { get; set; } = string.Empty;
+
+        public static void LoadMedia(string[] playlist)
+        {
+            foreach (string media in playlist) Media.Add(media);
+            Current = playlist[0];
+        }
+    }
+}
